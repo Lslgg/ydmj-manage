c42aeb515f1433d06be70b63e619e32901e1245e
luke123321 <437032704@qq.com>
2018-03-09 18:09:36 +0800

    ok

--- src/app/admin/mall/business/component/addBusiness.component.ts
+++ src/app/admin/mall/business/component/addBusiness.component.ts
@@ -11,15 +11,15 @@ import gql from 'graphql-tag';
 export class AddBusinessComponent implements OnInit {
 
     businessForm: FormGroup = this.fb.group({
-        id: [''],        
+        id: [''],
         name: ['', Validators.required],
-        address: ['', Validators.required],                  
+        address: ['', Validators.required],
         phone: ['', Validators.required],
         hours: ['', Validators.required],
         brief: ['', Validators.required],
         imageIds: ['', Validators.required],
         times: [0, Validators.required],
-        score: [0, Validators.required],    
+        score: [0, Validators.required],
         isValid: [true],
     });
 
@@ -43,7 +43,7 @@ export class AddBusinessComponent implements OnInit {
 
     files: Array<any> = new Array<any>();
 
-    constructor( @Inject("commonData") private cdate: CommonData,
+    constructor(@Inject("commonData") private cdate: CommonData,
         private fb: FormBuilder, private route: ActivatedRoute,
         private router: Router) {
     }
