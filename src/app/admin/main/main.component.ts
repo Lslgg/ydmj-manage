a7681414ab2407e2e16c527518e5c621272e31d7
luke123321 <437032704@qq.com>
2018-03-16 17:40:52 +0800

    ok

--- src/app/admin/main/main.component.ts
+++ src/app/admin/main/main.component.ts
@@ -14,17 +14,17 @@ export class MainComponent {
 
   constructor(@Inject("commonData") private cdate: CommonData,
     private apollo: Apollo) {
-    var sql = gql`query($createTime: String!){
-        totalPlayer:getPlayerCount(where:"1=1")
-        onlinePlayer:getPlayerCount(where:"state =1")
-        newPlayer:getPlayerCount(where:$createTime)
-        useCard:getCardRecordStatistice(where:$createTime)
-      }`;
-    var nowDate = this.cdate.toDateFormat();
-    var where = { createTime: `FROM_UNIXTIME(createTime/1000)>='${nowDate}'` };
-    var qInfo = { query: sql, variables: where };
-    this.apollo.query(qInfo).subscribe(({ data }) => {
-      Object.assign(this.total, data);
-    })
+    // var sql = gql`query($createTime: String!){
+    //     totalPlayer:getPlayerCount(where:"1=1")
+    //     onlinePlayer:getPlayerCount(where:"state =1")
+    //     newPlayer:getPlayerCount(where:$createTime)
+    //     useCard:getCardRecordStatistice(where:$createTime)
+    //   }`;
+    // var nowDate = this.cdate.toDateFormat();
+    // var where = { createTime: `FROM_UNIXTIME(createTime/1000)>='${nowDate}'` };
+    // var qInfo = { query: sql, variables: where };
+    // this.apollo.query(qInfo).subscribe(({ data }) => {
+    //   Object.assign(this.total, data);
+    // })
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // dataServer: 'http://127.0.0.1:8080'
-  dataServer:'http://localhost:8080'
+  dataServer: 'http://127.0.0.1:8080'
+  // dataServer:'http://localhost:8080'
 };
